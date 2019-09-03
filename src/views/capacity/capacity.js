3fc41baf3aebd03d8ee53f7819e2936676917edb
coeneivan <ivan@ivancoene.be>
2019-09-03 16:22:24 +0200

    WIP capacity page

--- src/views/capacity/capacity.js
+++ src/views/capacity/capacity.js
@@ -1,6 +1,9 @@
+import miniGraph from '../../components/minigraph'
+import capacitymap from '../../components/capacitymap'
+import scrollablecard from '../../components/scrollablecard'
 export default {
   name: 'capacity',
-  components: {},
+  components: { miniGraph, capacitymap, scrollablecard },
   props: [],
   data () {
     return {
