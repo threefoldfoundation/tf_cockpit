291efe49faa33a1c0e7cdec8c4ebcedb5f053178
Maxime Daniel <root@maxux.net>
2019-10-08 02:00:09 +0200

    Update commented api endpoint

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 export default ({
   // tfApiUrl: 'https://radar.threefold.io/api/'
-  // tfApiUrl: 'http://bcdb.test.grid.tf:9201/actors/'
+  // tfApiUrl: 'http://bcdb.test.grid.tf/api/actors/'
   tfApiUrl: 'http://localhost:9999/actors/'
 })
