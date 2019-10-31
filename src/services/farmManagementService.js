747cfac1e577d9633bb96123ffa62ca9a5e93cbf
Christophe de Carvalho <christophe.dcpm@gmail.com>
2019-10-31 08:50:06 +0100

    update API url to the explorer testnet address

--- src/services/farmManagementService.js
+++ src/services/farmManagementService.js
@@ -2,18 +2,18 @@ import Axios from 'axios'
 import config from '../../public/config'
 
 export default ({
-  getFarms () {
-    return Axios.post(`${config.jsApiUrl}farms/list`)
+  getFarms() {
+    return Axios.post(`${config.tfApiUrl}farms/list`)
   },
-  registerFarm (farm) {
-    return Axios.post(`${config.jsApiUrl}farms/register`, {
+  registerFarm(farm) {
+    return Axios.post(`${config.tfApiUrl}farms/register`, {
       args: {
         farm
       }
     })
   },
-  updateFarm (farm_id, farm) {
-    return Axios.post(`${config.jsApiUrl}farms/update`, {
+  updateFarm(farm_id, farm) {
+    return Axios.post(`${config.tfApiUrl}farms/update`, {
       args: {
         farm_id,
         farm
